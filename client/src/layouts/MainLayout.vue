6a1dfbcbf84982309f15b8f47c67e2fb7dda030e
dhsuioa <a27litvinenko@gmail.com>
2024-01-10 01:25:32 +0300

    init

---
+++ client/src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated style="background-color: #FFFFFF">
+      <q-toolbar>
+        <q-toolbar-title >
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container style="background-color: #F5F5F5">
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+
+</script>
