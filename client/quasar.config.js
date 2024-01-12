3001f6bd2af6356a24ba1b062f71ff37fd1c0533
Alexandr Litvinenko <a27litvinenko@gmail.com>
2024-01-12 19:44:54 +0300

    Update quasar.config.js

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/VkApi',
+      publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
