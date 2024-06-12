c8439144dbff2c57c9a7745702010670bda8da51
Alexandr Litvinenko <a27litvinenko@gmail.com>
2024-06-12 16:04:29 +0300

    header fix

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -1,15 +1,33 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
     <!-- <q-header elevated style="background-color: #FFFFFF"> -->
     <q-header style="background-color: #FFFFFF">
       <q-toolbar>
-        <q-toolbar-title >
-          <span :class="` text-accent`">PsyNet</span>
+        <q-toolbar-title class="row">
+          <span :class="`text-accent`">PsyNet</span>
           <span :class="`text-primary`">Vk</span>
+          <div class="q-gutter-sm q-ml-sm" v-for="link in links">
+            <q-btn
+              :label="link.title"
+              :to="link.to"
+              color="dark"
+              flat
+              rounded
+            />
+          </div>
         </q-toolbar-title>
         <q-space />
-        <q-btn>asd</q-btn>
+        <q-btn-dropdown outline label="@id" color="dark" dropdown-icon="false">
+          <q-list class="text-negative">
+            <q-item  clickable v-close-popup @click="onItemClick">
+              <q-item-section>
+                <q-item-label>
+                  Выход
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
 
@@ -21,5 +39,10 @@
 </template>
 
 <script setup lang="ts">
+const links = [
+  { title: 'Главная', to: '/' },
+  { title: 'Анализ', to: '/analysis' },
+  { title: 'Результаты', to: '/results' }
+]
 
 </script>
